3aaed7ade4a527519787f59c84a9220292d8d477
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-20 05:22:35 +0500

    Timer-Downward

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
 
 function App() {
+  const [time , setTime]= useState(25);
+  const [interv , setInterv] = useState();
+  const [color ,setColor] = useState();
+
+ const start = () =>{
+   run();
+   setInterv(setInterval(run,1000));
+ };
+ const reset = () => {
+   clearInterval(interv);
+   setTime(25)
+ };
+ var timer = time;
+ const run = ()=>{
+   timer--;
+   if(timer===20){
+     setColor('black');
+   }
+   if(timer===10){
+   setColor('yellow');
+   }
+   if(timer===5){
+     setColor('white')
+ }
+ if(timer>=0){
+   return setTime(timer)
+ }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timer" style={{backgroundColor:color}}>
+    <h3>{time}</h3>
+      <button onClick={()=>start()}>Click</button>
+      <button onClick={()=>reset()}>Reset</button>
     </div>
   );
 }
